eddf38c2f1f76e7dbeb1b4e8eca9f33fb4f00e61
Izzet Mustafaiev <webdizz@gmail.com>
2013-10-22 09:33:20 +0300

    Unnecessary statements were removed

--- buildfile.rb
+++ buildfile.rb
@@ -3,12 +3,10 @@ GROUP = "junit"
 
 repositories.remote << "http://repo1.maven.org/maven2"
 
-desc "The Junit project"
 define "junit" do
-
   project.version = VERSION_NUMBER
   project.group = GROUP
   compile.with 'org.hamcrest:hamcrest-core:jar:1.3'
   test.compile.with 
-  package(:jar)
+  package :jar
 end
